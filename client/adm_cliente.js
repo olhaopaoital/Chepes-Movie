2310cf7956f27689f4bedaff79a1711c776afd3f
CaueAlvim <caue.eyti@gmail.com>
2023-09-03 18:38:21 -0300

    javascript para criação de user

--- client/adm_cliente.js
+++ client/adm_cliente.js
@@ -5,3 +5,41 @@ function toggleSortIcon(sortImg) {
         sortImg.src = './icons/arrow-fat-up.svg';
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const salvarUsuarioButton = document.getElementById("salvarUsuario");
+
+    salvarUsuarioButton.addEventListener("click", function () {
+        const userData = {
+            firstName: document.getElementById("firstName").value,
+            lastName: document.getElementById("lastName").value,
+            birthDate: document.getElementById("birthDate").value,
+            cpf: document.getElementById("cpf").value,
+            email: document.getElementById("email").value,
+            password: document.getElementById("password").value,
+            phoneNumber: document.getElementById("phoneNumber").value,
+            genre: document.getElementById("gender").value
+        };
+        console.log(userData);
+
+        // Envie os dados para a sua API Node.js usando uma requisição HTTP POST
+        fetch("http://localhost:3333/app/user/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userData),
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Aqui você pode lidar com a resposta da API, como exibir uma mensagem de sucesso
+            console.log("Resposta da API:", data);
+            alert("Usuário criado com sucesso!");
+        })
+        .catch(error => {
+            // Lidar com erros, como exibir uma mensagem de erro
+            console.error("Erro ao criar usuário:", error);
+            alert("Erro ao criar usuário. Verifique os dados e tente novamente.");
+        });
+    });
+});
